8b9e0987c78ce8e7d650bf1d5b86b8d86727e10c
Fayola Menezes <fayolamenezes16@gmail.com>
2025-05-26 23:41:30 +0530

    commit

--- script.js
+++ script.js
@@ -23,6 +23,9 @@ text.split("").forEach(char => {
   roadText.appendChild(span);
 });
 
+gsap.set(roadText, { opacity: 1 });
+gsap.set(roadText.querySelectorAll("span"), { opacity: 0 });
+
 let scrollTween;
 
 function createAnimation() {
